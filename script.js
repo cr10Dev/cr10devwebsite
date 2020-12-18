7ef2d8d2c161e73a530aadd993d13ce4d9201ce6
cr10Dev <75941095+cr10Dev@users.noreply.github.com>
2020-12-18 15:30:54 +0000

    Updated some stuff, better structure, semi-functional partials

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-async function loadPartials() {
+async function loadPartials(partialPath) {
     leJSON = undefined
-    await $.getJSON("partials.json", function(json) {
+    await $.getJSON(partialPath, function(json) {
     leJSON = json
-    })
+    });
 
     for (i = 0; i < document.getElementsByClassName("navbar-insert").length; i++) document.getElementsByClassName("navbar-insert")[i].innerHTML = leJSON.navbar;
     //for (i = 0; i < document.getElementsByClassName("footer").length; i++) document.getElementsByClassName("footer")[i] = leJSON.footer;
